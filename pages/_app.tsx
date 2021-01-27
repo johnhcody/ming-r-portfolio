fd17107222901932751a50ff0c09e4bd01cd53b5
johnhcody <john.haner.cody@gmail.com>
2021-01-27 15:47:38 +0700

    updated node, sass, and tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProps } from 'next/app'
 import '../styles/application.scss'
+import "tailwindcss/tailwind.css"
 import { LanguageProvider } from '../context/languageContext'
 import { PhotoProvider } from '../context/photoContext'
 
