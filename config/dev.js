26f94ae9f14c81e2c5745f1698ffe8e4ffd24101
johnhcody <john.haner.cody@gmail.com>
2021-02-04 11:54:21 +0700

    adjust keys

---
+++ config/dev.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        MONGO_URI: `mongodb+srv://johnhcody:tux9YTLl7qpL3Khl@cluster0.ylm0g.mongodb.net/ming-portfolio?retryWrites=true&w=majority`,
+        ACCESS_KEY: 'AKIATXFIPXWOMG6BQ3Q5',
+        SECRET_KEY: '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',
+        REGION: 'ap-northeast-2',
+        BUCKET_NAME: 'ming-portfolio-uploads',
+        AUTH0_DOMAIN: 'ming-portfolio.jp.auth0.com',
+        AUTH0_CLIENT_ID: 'RCgMq5ADQR8POLG7sKLsMbwdL2CIvTV2',
+        AUTH0_CLIENT_SECRET: 'OMugu1I0M5L7M7h0Sbm4Yf48d0PpNYyqpICUdCRRyLuXBx41U5MkNB1o6kecilgt',
+        AUTH0_REDIRECT_URI: 'http://localhost:3000/api/callback',
+        AUTH0_POST_LOGOUT_REDIRECT_URI: 'http://localhost:3000',
+        AUTH0_COOKIE_SECRET: 'whqK4zathq2UQxOOdTjDY0BrNCGRV3bx'
+
+    }
+}
