0ab5ad509e7d3e4f09b2360120f70fc2b844eee9
johnhcody <john.haner.cody@gmail.com>
2021-02-04 21:07:19 +0700

    crud test

--- components/splash/IndexFooter.tsx
+++ components/splash/IndexFooter.tsx
@@ -13,13 +13,13 @@ const IndexFooter:React.FC<Props> = (props: Props) => {
     if (!data) {
         return (
             <div className="login-wrapper">
-                <a href="api/login">Admin Login</a>
+                <a className="font-sans" href="api/login">Admin Login</a>
             </div>
         )
     } else if (data && data.name == "john.haner.cody@gmail.com"){
         return (
             <div className="login-wrapper">
-                <a href="api/logout">Logout</a>
+                <a  className="font-sans" href="api/logout">Logout</a>
             </div>
             )
     } else {
--- pages/create-new.tsx
+++ pages/create-new.tsx
@@ -28,7 +28,7 @@ const CreateNew:React.FC = props => {
     //     'message': '',
     //     'type': ''
     // });
-    const [ errors, setErrors ] = useState(null)
+    const [ errors, setErrors ] = useState({})
     const [ type, setType ] = useState('');
 
 
@@ -200,7 +200,7 @@ const CreateNew:React.FC = props => {
             ...form,
             'type': projectType 
         })
-        if (errors['type'] && projectType) delete errors['type']
+        if (errors && errors['type'] && projectType) delete errors['type']
         
         if (Object.values(errors).length == 1 && errors.message) delete errors['message']
     }
@@ -289,9 +289,9 @@ const CreateNew:React.FC = props => {
                             return <BlogParagraphUpload key={idx} inputIndex={idx} paragraphNumber={ipt} deleteElement={deleteParagraph} addText={handleTextInput}/>
                         }
                         })}
-                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full" onClick={appendPhoto}>Add Photo</button>    
-                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none" onClick={appendParagraph}>Add Paragraph</button>
-                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none" type="submit" onClick={postBlog} > Post {`${form.type}`}</button>
+                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full font-sans" onClick={appendPhoto}>Add Photo</button>    
+                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none font-sans" onClick={appendParagraph}>Add Paragraph</button>
+                        <button className="focus:outline-none focus:ring focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none font-sans" type="submit" onClick={postBlog} > Post {`${form.type}`}</button>
                         {errors && errors.message ? <h1 className="pt-2 text-red-500">{errors.message}</h1> : null}
                     </form>
                 </div>
