680900c2fb81b711763a7d5b26762c73fbcf09bf
johnhcody <john.haner.cody@gmail.com>
2021-02-05 09:43:40 +0700

    fix styling

--- components/shared/Upload.jsx
+++ components/shared/Upload.jsx
@@ -35,7 +35,7 @@ export default function Upload(props) {
     if (success) {
         return (
             <div className="upload-wrapper">
-                <p className="py-4">Uploaded Successfully</p>
+                <p className="py-4 font-sans">Uploaded Successfully</p>
                 <svg className="ml-3" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100.2 120.2">
                 <circle className="path circle" fill="none" stroke="#73AF55" strokeWidth="6" strokeMiterlimit="10" cx="45.1" cy="45.1" r="42.1"/>
                 <polyline className="path check" fill="none" stroke="#73AF55" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" points="60.2,30.2 38.5,65.8 25.8,50.5 "/>
@@ -49,7 +49,7 @@ export default function Upload(props) {
                     <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                     </svg>
-                    <span className="mt-2 font-sans text-base leading-normal">Select a file</span>
+                    <span className="mt-2 text-base leading-normal">Select a file</span>
                     <input 
                         onChange={uploadPhoto} 
                         title=" " 
