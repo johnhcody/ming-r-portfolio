8145c24cb56643581aa08c15a77e19034b0e1f57
johnhcody <john.haner.cody@gmail.com>
2021-02-05 09:17:19 +0700

    merging with master

--- pages/create-new.tsx
+++ pages/create-new.tsx
@@ -270,19 +270,19 @@ const CreateNew:React.FC = props => {
                     <form className="flex flex-col items-center w-4/5" onSubmit={handleSubmit}>
                         <h1 className="text-4xl">Create a New Blog Post</h1>
                         <Dropdown sendType={handleType}/>
-                        {errors && errors.type ? <h1 className="text-2xl text-red-500">{errors.type}</h1> : null }
-                        <label className="text-2xl pt-4 pb-2" htmlFor="title">{form.type} Title</label>
-                            <input type="text" onChange={handleChange} className="text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Grab their attention!" name="title"  />
-                        {errors && errors.title ? <h1 className="text-2xl text-red-500">{errors.title}</h1> : null}
-                        <label className="text-2xl pt-4 pb-2" htmlFor="intro">{form.type} Introduction</label>
-                            <textarea onChange={handleChange} className="border-2 rounded-md text-center w-full h-24 p-3 my-3" placeholder="Tell us a bit about your work!  This will appear on the main page." name="intro"  />
-                        <label className="text-2xl pt-4 pb-2" htmlFor="description">{form.type} Description</label>
-                            <textarea onChange={handleChange} className="border-2 rounded-md text-center w-full h-72 p-3 my-3" placeholder="Go into more detail about the project.  This will appear when people view the specific project." name="description"  />
-                            <label className="text-2xl pt-4 pb-2" htmlFor="linkUrl">{form.type} Link URL</label>
-                        <input type="text" onChange={handleChange} className="text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Copy and Paste Link Address" name="linkUrl"  />
-                            <label className="text-2xl pt-4 pb-2" htmlFor="linkDescription">{form.type} Link Description</label>
-                        <input type="text" onChange={handleChange} className="text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Text that will appear on hyperlink" name="linkDescription"  />
-                        <span className="text-2xl pt-4 pb-2" >Upload your Main Photo</span>
+                        {errors && errors.type ? <h1 className="font-sans text-2xl text-red-500">{errors.type}</h1> : null }
+                        <label className="font-sans text-2xl pt-4 pb-2" htmlFor="title">{form.type} Title</label>
+                            <input type="text" onChange={handleChange} className="font-sans text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Grab their attention!" name="title"  />
+                        {errors && errors.title ? <h1 className="font-sans text-2xl text-red-500">{errors.title}</h1> : null}
+                        <label className="font-sans text-2xl pt-4 pb-2" htmlFor="intro">{form.type} Introduction</label>
+                            <textarea onChange={handleChange} className="font-sans border-2 rounded-md text-center w-full h-24 p-3 my-3" placeholder="Tell us a bit about your work!  This will appear on the main page." name="intro"  />
+                        <label className="font-sans text-2xl pt-4 pb-2" htmlFor="description">{form.type} Description</label>
+                            <textarea onChange={handleChange} className="font-sans border-2 rounded-md text-center w-full h-72 p-3 my-3" placeholder="Go into more detail about the project.  This will appear when people view the specific project." name="description"  />
+                            <label className="font-sans text-2xl pt-4 pb-2" htmlFor="linkUrl">{form.type} Link URL</label>
+                        <input type="text" onChange={handleChange} className="font-sans text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Copy and Paste Link Address" name="linkUrl"  />
+                            <label className="font-sans text-2xl pt-4 pb-2" htmlFor="linkDescription">{form.type} Link Description</label>
+                        <input type="text" onChange={handleChange} className="font-sans text-center w-72 border-b-2 focus:outline-none border-t-0 border-l-0 border-r-0 mb-4" placeholder="Text that will appear on hyperlink" name="linkDescription"  />
+                        <span className="font-sans text-2xl pt-4 pb-2" >Upload your Main Photo</span>
                         <Upload name={"mainPhoto"} sendPhotoString={handleMainPhoto} title={"Main Photo"} />
                         {input.map((ipt, idx) => {
                         let word = ipt.slice(0,5);
@@ -293,9 +293,9 @@ const CreateNew:React.FC = props => {
                             return <BlogParagraphUpload key={idx} inputIndex={idx} paragraphNumber={ipt} deleteElement={deleteParagraph} addText={handleTextInput}/>
                         }
                         })}
-                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full font-sans" onClick={appendPhoto}>Add Photo</button>    
-                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none font-sans" onClick={appendParagraph}>Add Paragraph</button>
-                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none font-sans" type="submit" onClick={postBlog} > Post {`${form.type}`}</button>
+                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full" onClick={appendPhoto}>Add Photo</button>    
+                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:nones" onClick={appendParagraph}>Add Paragraph</button>
+                        <button className="focus:outline-none focus:ring font-sans focus:border-gray-300 bg-blue hover:bg-yellow-500 text-white font-bold py-2 px-4 m-2 rounded-full outline:none" type="submit" onClick={postBlog} > Post {`${form.type}`}</button>
                         {errors && errors.message ? <h1 className="pt-2 text-red-500">{errors.message}</h1> : null}
                     </form>
                 </div>
